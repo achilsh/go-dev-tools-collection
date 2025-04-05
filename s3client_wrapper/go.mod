4d750d0468c6f2301a68d3a44a45692ba086bf4b
achilsh <hwshtongxin@126.com>
2025-04-05 13:21:05 +0800

    add s3 client gov2 wrapper

--- s3client_wrapper/go.mod
+++ s3client_wrapper/go.mod
@@ -4,4 +4,7 @@ go 1.24.1
 
 require github.com/aws/aws-sdk-go v1.55.6
 
-require github.com/jmespath/go-jmespath v0.4.0 // indirect
+require (
+	github.com/aws/aws-sdk-go-v2 v1.36.3 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ s3client_wrapper/lib/v2/s3SdkWrapper.go
@@ -0,0 +1,10 @@
+package s3SdkV2
+
+import (
+	_ "context"
+
+	_ "github.com/aws/aws-sdk-go-v2/config"
+)
+
+//参考： https://docs.aws.amazon.com/zh_cn/sdk-for-go/v2/developer-guide/go_s3_code_examples.html
+//参考： https://github.com/awsdocs/aws-doc-sdk-examples/tree/main/gov2
